161c360b4c9645af097a15142ee3ed1fa1052113
FIREGAMERZ <104581809+FIREGAMERZ@users.noreply.github.com>
2024-03-08 15:17:46 +0530

    index.py

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ from movies_scraper import search_movies, get_movie
 
 
 TOKEN = os.getenv("TOKEN")
-URL = "https://hd-movie-bot-amq3.vercel.app"
+URL = "https://hd-movie-bot-9hea.vercel.app"
 bot = Bot(TOKEN)
 
 
