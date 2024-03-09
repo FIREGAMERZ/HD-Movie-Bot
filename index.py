1fb8b30a549552a13851f63277b84690aa4a92bd
FIREGAMERZ <104581809+FIREGAMERZ@users.noreply.github.com>
2024-03-10 00:48:00 +0530

    index.py

--- index.py
+++ index.py
@@ -1,85 +1,86 @@
 import os
-from io import BytesIO
-from queue import Queue
-import requests
 from flask import Flask, request
-from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
-from movies_scraper import search_movies, get_movie
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext, CallbackQueryHandler
+import requests
+from bs4 import BeautifulSoup
 
+TOKEN = "7139621410:AAEWNXk8JQFmQTpSbNBbUZ7gl9Zk45qIkgo"
+app = Flask(__name__)
+updater = Updater(TOKEN, use_context=True)
+dispatcher = updater.dispatcher
 
-TOKEN = os.getenv("TOKEN")
-URL = "https://replit.com/@joshuapillai777/HD-Movie-Bot?s=app"
-bot = Bot(TOKEN)
+url_list = {}
+api_key = "44a1bfad3948393badd4d5e4e7ec9b81f39282af"
 
+def search_movies(query):
+    movies_list = []
+    website = BeautifulSoup(requests.get(f"https://mkvcinemas.skin/?s={query.replace(' ', '+')}").text, "html.parser")
+    movies = website.find_all("a", {'class': 'ml-mask jt'})
+    for movie in movies:
+        if movie:
+            title = movie.find("span", {'class': 'mli-info'}).text
+            movies_list.append(title)
+            url_list[title] = movie['href']
+    return movies_list
 
-def welcome(update, context) -> None:
+def get_movie(query):
+    movie_details = {}
+    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
+    if movie_page_link:
+        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
+        movie_details["title"] = title
+        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
+        final_links = {}
+        for i in links:
+            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
+            response = requests.get(url)
+            if response.status_code == 200:
+                link = response.json()
+                final_links[f"{i.text}"] = link['shortenedUrl']
+        movie_details["links"] = final_links
+    return movie_details
+
+def welcome(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to SB Movies.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
 
-
-def find_movie(update, context):
-    search_results = update.message.reply_text("Processing...")
+def find_movie(update: Update, context: CallbackContext) -> None:
     query = update.message.text
     movies_list = search_movies(query)
     if movies_list:
-        keyboards = []
-        for movie in movies_list:
-            keyboard = InlineKeyboardButton(movie["title"], callback_data=movie["id"])
-            keyboards.append([keyboard])
-        reply_markup = InlineKeyboardMarkup(keyboards)
-        search_results.edit_text('Search Results...', reply_markup=reply_markup)
+        reply_markup = InlineKeyboardMarkup.from_column(
+            [[InlineKeyboardButton(title, callback_data=title)] for title in movies_list])
+        update.message.reply_text('Search Results...', reply_markup=reply_markup)
     else:
-        search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
+        update.message.reply_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
 
-
-def movie_result(update, context) -> None:
+def movie_result(update: Update, context: CallbackContext) -> None:
     query = update.callback_query
     s = get_movie(query.data)
-    response = requests.get(s["img"])
-    img = BytesIO(response.content)
-    query.message.reply_photo(photo=img, caption=f"🎥 {s['title']}")
-    link = ""
-    links = s["links"]
-    for i in links:
-        link += "🎬" + i + "\n" + links[i] + "\n\n"
-    caption = f"⚡ Fast Download Links :-\n\n{link}"
-    if len(caption) > 4095:
-        for x in range(0, len(caption), 4095):
-            query.message.reply_text(text=caption[x:x+4095])
-    else:
-        query.message.reply_text(text=caption)
+    caption = f"🎥 {s['title']}\n\n"
+    for title, link in s["links"].items():
+        caption += f"🎬 {title}: {link}\n"
+    query.message.reply_text(text=caption)
 
-
-def setup():
-    update_queue = Queue()
-    dispatcher = Dispatcher(bot, update_queue, use_context=True)
-    dispatcher.add_handler(CommandHandler('start', welcome))
-    dispatcher.add_handler(MessageHandler(Filters.text, find_movie))
-    dispatcher.add_handler(CallbackQueryHandler(movie_result))
-    return dispatcher
-
-
-app = Flask(__name__)
-
+dispatcher.add_handler(CommandHandler('start', welcome))
+dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, find_movie))
+dispatcher.add_handler(CallbackQueryHandler(movie_result))
 
 @app.route('/')
 def index():
     return 'Hello World!'
 
-
-@app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
-def respond():
-    update = Update.de_json(request.get_json(force=True), bot)
-    setup().process_update(update)
-    return 'ok'
-
-
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('{URL}/{HOOK}'.format(URL=URL, HOOK=TOKEN))
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+    # Logic to set up webhook
+    return 'Webhook setup successful'  # Or any appropriate response
+
+def webhook():
+    update = Update.de_json(request.get_json(force=True), updater.bot)
+    dispatcher.process_update(update)
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(port=5000)
